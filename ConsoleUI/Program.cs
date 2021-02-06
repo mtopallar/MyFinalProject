60a6fe61b6d9bcac7d0e1a12db4d00c2f5fd5ad4
Murat Topallar <topallarm@outlook.com>
2021-02-07 00:48:30 +0300

    Category adında da "/" olan ürünler olduğu için console da  "/" yerine "---" eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,7 +32,7 @@ namespace ConsoleUI
 
             foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName+ "/"+product.CategoryName);
+                Console.WriteLine(product.ProductName+ " --- "+product.CategoryName);
             }
         }
     }
