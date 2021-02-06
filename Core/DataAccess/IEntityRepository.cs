e7f0469a1ac5201e154df8c08d0f2510c41895aa
Murat Topallar <topallarm@outlook.com>
2021-02-06 23:55:34 +0300

    6 Şubat Ders Sonu

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
-using Entities.Abstract;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
+    //Core katmanı diğer katmanları referans almaz. Alırsa sen o katmana bağımlısın demek.
     public interface IEntityRepository<T> where T : class, IEntity, new() //generic constraint
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
