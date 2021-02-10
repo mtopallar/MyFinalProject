329c3f132f1b70d9349384229b2a03de6ac91392
Murat Topallar <topallarm@outlook.com>
2021-02-11 00:04:28 +0300

    10 Şubat Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages //norhwind ile ilgili mesajları verdiğimiz class. Her projede ürün yoktur onun için bu klasörü core a oluşturmadık. Evransel değil northwind e özgü.
+    {
+        public static string ProductAdded = "Ürün eklendi"; //Public ler büyük harfle başlar (pascal case)
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success) // 2 parametre gelirse tek parametreli success i de çalıştır. this bu sınıf demek. metoddan 2 parametre geldiğinde bu sınıfın tek parametreli ctor unu da tetiklemiş olduk.
+        {
+            Message = message;
+            // Get read only'dir. Ancak Ctor ile getter ı set edebilirsin. 
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+    
+}
