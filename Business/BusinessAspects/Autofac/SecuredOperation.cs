41df5ebf4c340565c214b05121e1155d02cdaf44
Murat Topallar <topallarm@outlook.com>
2021-10-27 01:13:26 +0300

    Sistem genelinde bazı notlar alındı.

--- Business/BusinessAspects/Autofac/SecuredOperation.cs
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -13,26 +13,27 @@ namespace Business.BusinessAspects.Autofac
 {
     //Authorization aspect leri genellikle business a yazılır. Çünkü projeye göre yetki lontrolü değişebilir.
     //SecuredOperation JWT için.
+    // Bu class Authorization Aspect kullanabilmek için oluşturduğumuz bir class. Authorization aspectleri genellikle business içinde yer alır. Çünkü her projenin yetkilendirme algoritması değişebilir. Altyapıyı Core içine yazıyoruz ama aspect kısmını genellikle Business içine yerleştiriyoruz.
     public class SecuredOperation:MethodInterception
     {
         private string[] _roles;
-        private IHttpContextAccessor _httpContextAccessor; //jwt ı da gönderere istek yapıldığında her istek için bir http context i (thread i) oluşur
+        private IHttpContextAccessor _httpContextAccessor; //İçinde JWT da olan her bir istekte (istek zarfında) her istek için bir http context i (thread i) oluşur.
 
         public SecuredOperation(string roles)
         {
-            _roles = roles.Split(','); //rolleri virgülden bölüp array e at
-            _httpContextAccessor =  ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+            _roles = roles.Split(','); //aspect içinde belirttiğimiz rolleri virgül ile bölüp array formatına getiriyoruz.
+            _httpContextAccessor =  ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>(); //JWT zincirin içinde olduğu için injection ile IConfiguration ı enjekte edip çalıştırabilmiştik. Ancak Aspect zincirin dışında olduğu için, bu satırla Autofac ile oluşturduğumuz servis mimarisine ulaş ve karşılığı al demiş oluyor. Örneğin elimizde productService olsa ve bir winform uygulası yazıyor olsak (dependency injection imkanımız olmasa)                                         productService = ServiceTool.ServiceProvider.GetService<IProdductService>();                    gibi IoC deki karşılığı alabilir ve kullanabiliriz. Provider olarak Autofac i verdiğimiz için Autofac deki karşılıkları alacaktır. (WebAPI deki Program.cs deki .UseServiceProviderFactory(new AutofacServiceProviderFactory())) 
 
         }
 
         protected override void OnBefore(IInvocation invocation)
         {
-            var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+            var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles(); //ClaimsPrincipalExtension dan faydalanarak JWT içindeki rolleri alıyoruz.
             foreach (var role in _roles)
             {
                 if (roleClaims.Contains(role))
                 {
-                    return; //yani metodu çalıştırmaya devam et.
+                    return; //aspectin bulunduğu metodu (mesela add) çalıştırmaya devam et.
                 }
             }
             throw new Exception(Messages.AuthorizationDenied);
--- Core/Extensions/ClaimsPrincipalExtensions.cs
+++ Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -7,15 +7,18 @@ using System.Text;
 namespace Core.Extensions
 {
     public static class ClaimsPrincipalExtensions //Claimsprincipal jwt ile gelen user claimlerini okumak için kullanılır.
+    /* bir kişinin claimlerini ararken .net biraz uğraştırır. JWT yani kullanıcının gönderdiği token içinde gelen claimleri okumak için claimsprincipal(claimlere erişmek için .net in kendi class ı) extend ettik. ilk metod claim type ı parametre olarak alır ve sonucu claim type a göre döndürürken ikinci metodda direk claimsPrincipal.ClaimRoles() ile direk rolleri dönüyoruz.*/
     {
         public static List<string> Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
         {
+            // mesela claim type olarak roles diyoruz ve roles claimlerini List<string> olarak dönüyor.
             var result = claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
             return result;
         }
 
         public static List<string> ClaimRoles(this ClaimsPrincipal claimsPrincipal)
         {
+            // yukarıdaki metod yardımıyla claimtype ı roles olan claimleri direk dönüyor.
             return claimsPrincipal?.Claims(ClaimTypes.Role);
         }
     }
